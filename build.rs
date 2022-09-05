e64e79d71fa81f3fcacd0a9e173c0c171624aca8
Andrei <andrei.i@posteo.de>
2022-09-05 00:00:00 +0000

    Rerun uniffi if changed

--- build.rs
+++ build.rs
@@ -2,8 +2,11 @@ use camino::Utf8Path;
 use std::env;
 
 fn main() {
+    let udl_file = Utf8Path::new("src/lipalightninglib.udl");
+    println!("cargo:rerun-if-changed={}", udl_file);
+
     uniffi_bindgen::generate_component_scaffolding(
-        Utf8Path::new("src/lipalightninglib.udl"),
+        udl_file,
         None,
         Some(Utf8Path::new(&env::var("OUT_DIR").unwrap())),
         false,
@@ -11,7 +14,7 @@ fn main() {
     .unwrap();
 
     uniffi_bindgen::generate_bindings(
-        Utf8Path::new("src/lipalightninglib.udl"),
+        udl_file,
         None,
         Vec::from(["swift"]),
         Some(Utf8Path::new("bindings/swift")),
@@ -21,7 +24,7 @@ fn main() {
     .unwrap();
 
     uniffi_bindgen::generate_bindings(
-        Utf8Path::new("src/lipalightninglib.udl"),
+        udl_file,
         None,
         Vec::from(["kotlin"]),
         Some(Utf8Path::new("bindings/kotlin")),
