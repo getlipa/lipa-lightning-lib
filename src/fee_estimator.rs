8c0064edada3efda1f06b5daa8c4f5fffc10b7ca
Andrei <92177534+andrei-21@users.noreply.github.com>
2022-09-19 00:00:00 +0000

    Stub fee estimator

---
+++ src/fee_estimator.rs
@@ -0,0 +1,45 @@
+use lightning::chain::chaininterface::{ConfirmationTarget, FeeEstimator as LdkFeeEstimator};
+
+pub struct FeeEstimator;
+
+impl LdkFeeEstimator for FeeEstimator {
+    fn get_est_sat_per_1000_weight(&self, confirmation_target: ConfirmationTarget) -> u32 {
+        // TODO: Implement.
+        match confirmation_target {
+            ConfirmationTarget::Background => 253,
+            ConfirmationTarget::Normal => 1000,
+            ConfirmationTarget::HighPriority => 2000,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fee_is_above_minimum() {
+        let client = FeeEstimator {};
+        assert!(client.get_est_sat_per_1000_weight(ConfirmationTarget::Background) >= 253);
+        assert!(client.get_est_sat_per_1000_weight(ConfirmationTarget::Normal) >= 253);
+        assert!(client.get_est_sat_per_1000_weight(ConfirmationTarget::HighPriority) >= 253);
+    }
+
+    #[test]
+    fn fee_is_reasonable() {
+        let client = FeeEstimator {};
+        assert!(client.get_est_sat_per_1000_weight(ConfirmationTarget::Background) < 1000000);
+        assert!(client.get_est_sat_per_1000_weight(ConfirmationTarget::Normal) < 5000000);
+        assert!(client.get_est_sat_per_1000_weight(ConfirmationTarget::HighPriority) < 10000000);
+    }
+
+    #[test]
+    fn fee_is_ordered() {
+        let client = FeeEstimator {};
+        let background = client.get_est_sat_per_1000_weight(ConfirmationTarget::Background);
+        let normal = client.get_est_sat_per_1000_weight(ConfirmationTarget::Normal);
+        let high_priority = client.get_est_sat_per_1000_weight(ConfirmationTarget::HighPriority);
+        assert!(background <= normal);
+        assert!(normal <= high_priority);
+    }
+}
