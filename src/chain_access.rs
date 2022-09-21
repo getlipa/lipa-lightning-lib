e6aab5f056c713a9c89425eddf6166c1737b2979
Daniel Granhão <daniel.granhao@gmail.com>
2022-09-21 14:13:26 +0100

    Improve ChainAccess tests

--- src/chain_access.rs
+++ src/chain_access.rs
@@ -69,14 +69,29 @@ mod tests {
     use lightning::chain::{Filter, WatchedOutput};
     use std::sync::Arc;
 
-    #[test]
-    fn filter_is_initialised_empty() {
+    fn build_filter() -> LipaChainAccess {
         // 9 is a discard port
         // See https://en.wikipedia.org/wiki/Port_(computer_networking)
         let builder = Builder::new("http://localhost:9");
         let esplora_client = Arc::new(builder.build_async().unwrap());
+        LipaChainAccess::new(esplora_client.clone())
+    }
 
-        let filter = LipaChainAccess::new(esplora_client.clone());
+    fn build_sample_tx() -> Transaction {
+        let tx_bytes = Vec::from_hex(
+            "02000000000101595895ea20179de87052b4046dfe6fd515860505d6511a9004cf12a1f93cac7c01000000\
+            00ffffffff01deb807000000000017a9140f3444e271620c736808aa7b33e370bd87cb5a078702483045022\
+            100fb60dad8df4af2841adc0346638c16d0b8035f5e3f3753b88db122e70c79f9370220756e6633b17fd271\
+            0e626347d28d60b0a2d6cbb41de51740644b9fb3ba7751040121028fa937ca8cba2197a37c007176ed89410\
+            55d3bcb8627d085e94553e62f057dcc00000000"
+        ).unwrap();
+        let tx: Result<Transaction, _> = deserialize(&tx_bytes);
+        tx.unwrap()
+    }
+
+    #[test]
+    fn filter_is_initialised_empty() {
+        let filter = build_filter();
 
         assert!(filter.queued_txs.lock().unwrap().is_empty());
         assert!(filter.queued_outputs.lock().unwrap().is_empty());
@@ -86,41 +101,16 @@ mod tests {
 
     #[test]
     fn tx_is_registered() {
-        // 9 is a discard port
-        // See https://en.wikipedia.org/wiki/Port_(computer_networking)
-        let builder = Builder::new("http://localhost:9");
-        let esplora_client = Arc::new(builder.build_async().unwrap());
+        let filter = build_filter();
 
-        let filter = LipaChainAccess::new(esplora_client.clone());
-
-        // Get initial length of filter vecs
-        let initial_queued_tx_amount = filter.queued_txs.lock().unwrap().len();
-        let initial_queued_output_amount = filter.queued_outputs.lock().unwrap().len();
-        let initial_watched_tx_amount = filter.watched_txs.lock().unwrap().len();
-        let initial_watched_output_amount = filter.watched_outputs.lock().unwrap().len();
-
-        let tx_bytes = Vec::from_hex(
-            "02000000000101595895ea20179de87052b4046dfe6fd515860505d6511a9004cf12a1f93cac7c01000000\
-            00ffffffff01deb807000000000017a9140f3444e271620c736808aa7b33e370bd87cb5a078702483045022\
-            100fb60dad8df4af2841adc0346638c16d0b8035f5e3f3753b88db122e70c79f9370220756e6633b17fd271\
-            0e626347d28d60b0a2d6cbb41de51740644b9fb3ba7751040121028fa937ca8cba2197a37c007176ed89410\
-            55d3bcb8627d085e94553e62f057dcc00000000"
-        ).unwrap();
-        let tx: Result<Transaction, _> = deserialize(&tx_bytes);
-        let tx = tx.unwrap();
+        let tx = build_sample_tx();
 
         filter.register_tx(&tx.txid(), &tx.output[0].script_pubkey);
 
-        // Get final length of filter vecs
-        let final_queued_tx_amount = filter.queued_txs.lock().unwrap().len();
-        let final_queued_output_amount = filter.queued_outputs.lock().unwrap().len();
-        let final_watched_tx_amount = filter.watched_txs.lock().unwrap().len();
-        let final_watched_output_amount = filter.watched_outputs.lock().unwrap().len();
-
-        assert_eq!(final_queued_tx_amount, initial_queued_tx_amount + 1);
-        assert_eq!(final_watched_tx_amount, initial_watched_tx_amount);
-        assert_eq!(final_queued_output_amount, initial_queued_output_amount);
-        assert_eq!(final_watched_output_amount, initial_watched_output_amount);
+        assert_eq!(filter.queued_txs.lock().unwrap().len(), 1);
+        assert_eq!(filter.queued_outputs.lock().unwrap().len(), 0);
+        assert_eq!(filter.watched_txs.lock().unwrap().len(), 0);
+        assert_eq!(filter.watched_outputs.lock().unwrap().len(), 0);
 
         assert_eq!(
             filter.queued_txs.lock().unwrap().get(0).unwrap(),
@@ -130,28 +120,9 @@ mod tests {
 
     #[test]
     fn output_is_registered() {
-        // 9 is a discard port
-        // See https://en.wikipedia.org/wiki/Port_(computer_networking)
-        let builder = Builder::new("http://localhost:9");
-        let esplora_client = Arc::new(builder.build_async().unwrap());
+        let filter = build_filter();
 
-        let filter = LipaChainAccess::new(esplora_client.clone());
-
-        // Get initial length of filter vecs
-        let initial_queued_tx_amount = filter.queued_txs.lock().unwrap().len();
-        let initial_queued_output_amount = filter.queued_outputs.lock().unwrap().len();
-        let initial_watched_tx_amount = filter.watched_txs.lock().unwrap().len();
-        let initial_watched_output_amount = filter.watched_outputs.lock().unwrap().len();
-
-        let tx_bytes = Vec::from_hex(
-            "02000000000101595895ea20179de87052b4046dfe6fd515860505d6511a9004cf12a1f93cac7c01000000\
-            00ffffffff01deb807000000000017a9140f3444e271620c736808aa7b33e370bd87cb5a078702483045022\
-            100fb60dad8df4af2841adc0346638c16d0b8035f5e3f3753b88db122e70c79f9370220756e6633b17fd271\
-            0e626347d28d60b0a2d6cbb41de51740644b9fb3ba7751040121028fa937ca8cba2197a37c007176ed89410\
-            55d3bcb8627d085e94553e62f057dcc00000000"
-        ).unwrap();
-        let tx: Result<Transaction, _> = deserialize(&tx_bytes);
-        let tx = tx.unwrap();
+        let tx = build_sample_tx();
 
         let output = WatchedOutput {
             block_hash: None,
@@ -164,16 +135,10 @@ mod tests {
 
         filter.register_output(output.clone());
 
-        // Get final length of filter vecs
-        let final_queued_tx_amount = filter.queued_txs.lock().unwrap().len();
-        let final_queued_output_amount = filter.queued_outputs.lock().unwrap().len();
-        let final_watched_tx_amount = filter.watched_txs.lock().unwrap().len();
-        let final_watched_output_amount = filter.watched_outputs.lock().unwrap().len();
-
-        assert_eq!(final_queued_tx_amount, initial_queued_tx_amount);
-        assert_eq!(final_watched_tx_amount, initial_watched_tx_amount);
-        assert_eq!(final_queued_output_amount, initial_queued_output_amount + 1);
-        assert_eq!(final_watched_output_amount, initial_watched_output_amount);
+        assert_eq!(filter.queued_txs.lock().unwrap().len(), 0);
+        assert_eq!(filter.queued_outputs.lock().unwrap().len(), 0);
+        assert_eq!(filter.watched_txs.lock().unwrap().len(), 1);
+        assert_eq!(filter.watched_outputs.lock().unwrap().len(), 0);
 
         let filter_output = filter
             .queued_outputs
