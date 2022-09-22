8780db04218a03c52da9549d6519a31e1d016e9d
Gabriel Comte <gabriel.comte@gmail.com>
2022-09-22 09:32:34 +0200

    Sort InitializationErrors alphabetically

--- src/errors.rs
+++ src/errors.rs
@@ -1,22 +1,22 @@
 #[derive(Debug, thiserror::Error)]
 pub enum InitializationError {
+    #[error("Failed to start async runtime: {message}")]
+    AsyncRuntime { message: String },
+
+    #[error("Failed to add a channel monitor to the chain monitor")]
+    ChainMonitorWatchChannel,
+
+    #[error("Failed to read channel monitor backup: {message}")]
+    ChannelMonitorBackup { message: String },
+
+    #[error("Failed to create esplora client: {message}")]
+    EsploraClient { message: String },
+
     #[error("Failed to initialize keys manager: {message}")]
     KeysManager { message: String },
 
     #[error("Failed to generate random entropy: {message}")]
     SecretGeneration { message: String },
-
-    #[error("Failed to start async runtime: {message}")]
-    AsyncRuntime { message: String },
-
-    #[error("Failed to create esplora client: {message}")]
-    EsploraClient { message: String },
-
-    #[error("Failed to read channel monitor backup: {message}")]
-    ChannelMonitorBackup { message: String },
-
-    #[error("Failed to add a channel monitor to the chain monitor")]
-    ChainMonitorWatchChannel,
 }
 
 #[allow(dead_code)]
