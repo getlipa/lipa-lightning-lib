9b20985d60c9f34a48d66b1b61d3f5eb628e4404
Andrei <andrei.i@posteo.de>
2022-10-19 00:00:00 +0000

    Support zero-conf

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -1,6 +1,22 @@
-use lightning::util::events::{Event, EventHandler};
+use crate::types::ChannelManager;
 
-pub struct LipaEventHandler {}
+use bitcoin::secp256k1::PublicKey;
+use lightning::util::events::{Event, EventHandler};
+use std::sync::Arc;
+
+pub(crate) struct LipaEventHandler {
+    lsp_pubkey: PublicKey,
+    channel_manager: Arc<ChannelManager>,
+}
+
+impl LipaEventHandler {
+    pub fn new(lsp_pubkey: PublicKey, channel_manager: Arc<ChannelManager>) -> Self {
+        Self {
+            lsp_pubkey,
+            channel_manager,
+        }
+    }
+}
 
 impl EventHandler for LipaEventHandler {
     fn handle_event(&self, event: &Event) {
@@ -19,7 +35,27 @@ impl EventHandler for LipaEventHandler {
             Event::PaymentForwarded { .. } => {}
             Event::ChannelClosed { .. } => {}
             Event::DiscardFunding { .. } => {}
-            Event::OpenChannelRequest { .. } => {}
+            Event::OpenChannelRequest {
+                temporary_channel_id,
+                counterparty_node_id,
+                funding_satoshis: _,
+                push_msat: _,
+                channel_type,
+            } => {
+                if counterparty_node_id == &self.lsp_pubkey && channel_type.supports_zero_conf() {
+                    self.channel_manager
+                        .accept_inbound_channel_from_trusted_peer_0conf(
+                            temporary_channel_id,
+                            counterparty_node_id,
+                            0u64,
+                        )
+                        .unwrap();
+                } else {
+                    self.channel_manager
+                        .accept_inbound_channel(temporary_channel_id, counterparty_node_id, 0u64)
+                        .unwrap();
+                }
+            }
             Event::HTLCHandlingFailed { .. } => {}
         }
     }
