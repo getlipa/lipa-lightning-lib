ff3598dd18ddc7dcf86b9f1b531b26b9689a3ec0
Daniel Granhão <daniel.granhao@gmail.com>
2022-10-21 11:23:50 +0100

    Address review + wait for esplora to start

--- tests/chain_sync_test.rs
+++ tests/chain_sync_test.rs
@@ -10,47 +10,11 @@ mod chain_sync_test {
     use std::thread::sleep;
     use std::time::Duration;
 
+    use crate::setup::nigiri;
     use uniffi_lipalightninglib::config::NodeAddress;
 
-    fn fund_lnd_node(amount_btc: &str) -> Result<(), String> {
-        let output = setup::nigiri::exec(vec!["nigiri", "faucet", "lnd", amount_btc]);
-        if !output.status.success() {
-            return Err(format!("Command `faucet lnd {}` failed", amount_btc));
-        }
-        Ok(())
-    }
-
-    fn lnd_open_channel(node_id: &str) -> Result<(), String> {
-        let output = setup::nigiri::exec(vec![
-            "nigiri",
-            "lnd",
-            "openchannel",
-            "--private",
-            node_id,
-            "1000000",
-        ]);
-        if !output.status.success() {
-            return Err(format!(
-                "Command `lnd openchannel --private {} 1000000` failed",
-                node_id
-            ));
-        }
-        let _json: serde_json::Value =
-            serde_json::from_slice(&output.stdout).map_err(|_| "Invalid json")?;
-
-        Ok(())
-    }
-
-    fn mine_blocks(block_amount: &str) -> Result<(), String> {
-        let output = setup::nigiri::exec(vec!["nigiri", "rpc", "-generate", block_amount]);
-        if !output.status.success() {
-            return Err(format!("Command `rpc -generate {}` failed", block_amount));
-        }
-        Ok(())
-    }
-
     #[test]
-    fn test_channel_is_confirmed_chain() {
+    fn test_channel_is_confirmed_chain_after_6_confirmations() {
         setup::nigiri::start();
         let lsp_info = setup::nigiri::query_lnd_info().unwrap();
         let lsp_node = NodeAddress {
@@ -63,24 +27,21 @@ mod chain_sync_test {
 
         let node_id = node.get_node_info().node_pubkey.to_hex();
 
-        for i in 0..11 {
-            match fund_lnd_node("0.5") {
-                Ok(_) => break,
-                Err(_) => {
-                    if i < 10 {
-                        continue;
-                    }
-                }
-            }
-            fund_lnd_node("0.5").unwrap();
-        }
+        nigiri::try_function_multiple_times(
+            nigiri::fund_lnd_node,
+            0.5,
+            10,
+            Duration::from_millis(200),
+        )
+        .unwrap();
 
-        lnd_open_channel(&node_id).unwrap();
+        nigiri::lnd_open_channel(&node_id).unwrap();
 
         assert_eq!(node.get_node_info().num_channels, 1);
         assert_eq!(node.get_node_info().num_usable_channels, 0);
 
-        mine_blocks("6").unwrap();
+        nigiri::try_function_multiple_times(nigiri::mine_blocks, 6, 10, Duration::from_millis(200))
+            .unwrap();
 
         sleep(Duration::from_secs(10));
 
