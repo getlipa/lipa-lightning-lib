fcbca366b8e85d0759c0c530f662d61b93d8c6fc
Daniel Granhão <daniel.granhao@gmail.com>
2023-01-02 13:26:02 +0000

    Improve JIT channel opening tests

--- tests/receiving_payment_test.rs
+++ tests/receiving_payment_test.rs
@@ -14,11 +14,12 @@ mod receiving_payments_test {
     use crate::setup::{nigiri, NodeHandle};
     use crate::try_cmd_repeatedly;
 
-    const THOUSAND_SATS: u64 = 1_000_000;
-    const THREE_THOUSAND_SATS: u64 = 3_000_000;
+    const ONE_SAT: u64 = 1_000;
+    const ONE_K_SATS: u64 = 1_000_000;
+    const TWO_K_SATS: u64 = 2_000_000;
     const TEN_K_SATS: u64 = 10_000_000;
     const TWENTY_K_SATS: u64 = 20_000_000;
-    const MILLION_SATS: u64 = 1_000_000_000;
+    const FIVE_HUNDRED_K_SATS: u64 = 500_000_000;
 
     const HALF_SEC: Duration = Duration::from_millis(500);
     const N_RETRIES: u8 = 10;
@@ -29,7 +30,7 @@ mod receiving_payments_test {
     #[test]
     // Test receiving an invoice on a node that does not have any channel yet
     // resp, the channel opening is part of the payment process.
-    fn receive_payment_on_fresh_node() {
+    fn receive_payment_with_jit_channel_fresh_node() {
         let node_handle = NodeHandle::new_with_lsp_setup();
 
         let node = node_handle.start().unwrap();
@@ -45,19 +46,44 @@ mod receiving_payments_test {
         try_cmd_repeatedly!(nigiri::mine_blocks, N_RETRIES, HALF_SEC, 10);
         sleep(Duration::from_secs(10));
 
-        let invoice = node
-            .create_invoice(THREE_THOUSAND_SATS, "test".to_string())
-            .unwrap();
+        run_jit_channel_open_flow(
+            &node,
+            NodeInstance::NigiriLnd,
+            TWO_K_SATS + ONE_SAT,
+            TWO_K_SATS,
+        );
+    }
 
-        sleep(Duration::from_secs(5));
+    #[test]
+    fn receive_payment_with_jit_channel_existing_channels() {
+        let node = nigiri::initiate_node_with_channel(NodeInstance::LspdLnd);
+        run_payment_flow(&node, NodeInstance::LspdLnd, FIVE_HUNDRED_K_SATS);
 
-        nigiri::pay_invoice(NodeInstance::NigiriLnd, &invoice).unwrap();
+        // We have a 1M sat channel and have received a 0.5M payment. Another 0.5M payment is not
+        // possible due to channel reserves. A new channel with 0.6M size should be created
 
-        assert_eq!(node.get_node_info().channels_info.num_usable_channels, 1);
-        assert!(node.get_node_info().channels_info.local_balance_msat > 0);
+        let lspd_node_id = nigiri::query_node_info(NodeInstance::LspdLnd)
+            .unwrap()
+            .pub_key;
+
+        connect_node_to_lsp(NodeInstance::NigiriLnd, &lspd_node_id);
+
+        nigiri::lnd_node_open_pub_channel(NodeInstance::NigiriLnd, &lspd_node_id, false).unwrap();
+        try_cmd_repeatedly!(nigiri::mine_blocks, N_RETRIES, HALF_SEC, 10);
+        sleep(Duration::from_secs(10));
+
+        let initial_num_channels = node.get_node_info().channels_info.num_usable_channels;
+
+        run_jit_channel_open_flow(
+            &node,
+            NodeInstance::NigiriLnd,
+            FIVE_HUNDRED_K_SATS,
+            TWO_K_SATS,
+        );
+
         assert_eq!(
-            node.get_node_info().channels_info.local_balance_msat,
-            THOUSAND_SATS
+            node.get_node_info().channels_info.num_usable_channels,
+            initial_num_channels + 1
         );
     }
 
@@ -65,7 +91,7 @@ mod receiving_payments_test {
     // Test receiving an invoice on a node that already has an open channel
     fn receive_payment_on_established_node() {
         let node = nigiri::initiate_node_with_channel(NodeInstance::LspdLnd);
-        run_payment_flow(node, NodeInstance::LspdLnd, TWENTY_K_SATS, MILLION_SATS);
+        run_payment_flow(&node, NodeInstance::LspdLnd, TWENTY_K_SATS);
     }
 
     #[test]
@@ -74,13 +100,13 @@ mod receiving_payments_test {
     // A dust-HTLC is an HTLC that is too small to be worth the fees to settle it.
     fn receive_dust_htlc_payment_1k() {
         let node = nigiri::initiate_node_with_channel(NodeInstance::LspdLnd);
-        run_payment_flow(node, NodeInstance::LspdLnd, THOUSAND_SATS, MILLION_SATS);
+        run_payment_flow(&node, NodeInstance::LspdLnd, ONE_K_SATS);
     }
 
     #[test]
     fn receive_dust_htlc_payment_10k() {
         let node = nigiri::initiate_node_with_channel(NodeInstance::LspdLnd);
-        run_payment_flow(node, NodeInstance::LspdLnd, TEN_K_SATS, MILLION_SATS);
+        run_payment_flow(&node, NodeInstance::LspdLnd, TEN_K_SATS);
     }
 
     #[test]
@@ -99,7 +125,7 @@ mod receiving_payments_test {
             );
         }
 
-        assert_payment_received(&node, TWENTY_K_SATS * amt_of_payments, MILLION_SATS);
+        assert_payment_received(&node, TWENTY_K_SATS * amt_of_payments);
     }
 
     #[test]
@@ -123,7 +149,7 @@ mod receiving_payments_test {
         try_cmd_repeatedly!(nigiri::mine_blocks, N_RETRIES, HALF_SEC, 10);
         sleep(Duration::from_secs(10));
 
-        run_payment_flow(node, NodeInstance::NigiriLnd, TWENTY_K_SATS, MILLION_SATS);
+        run_payment_flow(&node, NodeInstance::NigiriLnd, TWENTY_K_SATS);
     }
 
     #[test]
@@ -147,7 +173,7 @@ mod receiving_payments_test {
         try_cmd_repeatedly!(nigiri::mine_blocks, N_RETRIES, HALF_SEC, 10);
         sleep(Duration::from_secs(110)); // wait for super lazy cln to consider its channels active
 
-        run_payment_flow(node, NodeInstance::NigiriCln, TWENTY_K_SATS, MILLION_SATS);
+        run_payment_flow(&node, NodeInstance::NigiriCln, TWENTY_K_SATS);
     }
 
     #[test]
@@ -179,21 +205,33 @@ mod receiving_payments_test {
         nigiri::lnd_pay_invoice(NodeInstance::NigiriLnd, &invoice).unwrap();
         nigiri::cln_pay_invoice(NodeInstance::NigiriCln, &invoice).unwrap();
 
-        assert_payment_received(&node, TWENTY_K_SATS * 3, MILLION_SATS);
+        assert_payment_received(&node, TWENTY_K_SATS * 3);
     }
 
-    fn run_payment_flow(
-        node: LightningNode,
+    fn run_jit_channel_open_flow(
+        node: &LightningNode,
         paying_node: NodeInstance,
         payment_amount: u64,
-        channel_size: u64,
+        lsp_fee: u64,
     ) {
+        let initial_balance = node.get_node_info().channels_info.local_balance_msat;
+
+        let invoice = issue_invoice(&node, payment_amount);
+
+        nigiri::pay_invoice(paying_node, &invoice).unwrap();
+
+        assert_payment_received(&node, initial_balance + payment_amount - lsp_fee);
+    }
+
+    fn run_payment_flow(node: &LightningNode, paying_node: NodeInstance, payment_amount: u64) {
+        let initial_balance = node.get_node_info().channels_info.local_balance_msat;
+
         assert_channel_ready(&node, payment_amount);
         let invoice = issue_invoice(&node, payment_amount);
 
         nigiri::pay_invoice(paying_node, &invoice).unwrap();
 
-        assert_payment_received(&node, payment_amount, channel_size);
+        assert_payment_received(&node, initial_balance + payment_amount);
     }
 
     fn assert_channel_ready(node: &LightningNode, payment_amount: u64) {
@@ -202,16 +240,13 @@ mod receiving_payments_test {
         assert!(node.get_node_info().channels_info.inbound_capacity_msat > payment_amount);
     }
 
-    fn assert_payment_received(node: &LightningNode, payment_amount: u64, channel_size: u64) {
+    fn assert_payment_received(node: &LightningNode, expected_balance: u64) {
         assert_eq!(
             node.get_node_info().channels_info.local_balance_msat,
-            payment_amount
+            expected_balance
         );
-        assert!(node.get_node_info().channels_info.outbound_capacity_msat < payment_amount); // because of channel reserves
-        assert!(
-            node.get_node_info().channels_info.inbound_capacity_msat
-                < channel_size - payment_amount
-        ); // smaller instead of equal because of channel reserves
+        assert!(node.get_node_info().channels_info.outbound_capacity_msat < expected_balance);
+        // because of channel reserves
     }
 
     fn issue_invoice(node: &LightningNode, payment_amount: u64) -> String {
