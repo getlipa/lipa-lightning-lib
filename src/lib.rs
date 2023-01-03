98b8eaf2299f4a82236f3c9a12975b839d234755
Gabriel Comte <gabriel.comte@gmail.com>
2023-01-03 14:30:11 +0100

    Refactor fn chomp_prefix to be more idiomatic

--- src/lib.rs
+++ src/lib.rs
@@ -517,8 +517,8 @@ impl LightningNode {
 
     fn chomp_prefix(string: &str) -> &str {
         let prefix = "lightning:";
-        if let Some(string) = string.strip_prefix(prefix) {
-            string
+        if let Some(payload) = string.strip_prefix(prefix) {
+            payload
         } else {
             string
         }
