42a03fdbf3fe8ab73af9c45e2d792997088e7dc3
Daniel Granhão <daniel.granhao@gmail.com>
2023-01-03 14:44:39 +0000

    Combine chain sync tests

--- tests/chain_sync_test.rs
+++ tests/chain_sync_test.rs
@@ -17,13 +17,13 @@ mod chain_sync_test {
     const N_RETRIES: u8 = 10;
 
     #[test]
-    fn test_channel_is_confirmed_only_after_6_confirmations() {
+    fn test_react_to_events() {
         let node_handle = NodeHandle::new_with_lsp_setup();
 
         let node = node_handle.start().unwrap();
         let node_id = node.get_node_info().node_pubkey.to_hex();
 
-        nigiri::lnd_node_open_channel(NodeInstance::LspdLnd, &node_id, false).unwrap();
+        let tx_id = nigiri::lnd_node_open_channel(NodeInstance::LspdLnd, &node_id, false).unwrap();
 
         assert_eq!(node.get_node_info().channels_info.num_channels, 1);
         assert_eq!(node.get_node_info().channels_info.num_usable_channels, 0);
@@ -41,71 +41,68 @@ mod chain_sync_test {
 
         assert_eq!(node.get_node_info().channels_info.num_channels, 1);
         assert_eq!(node.get_node_info().channels_info.num_usable_channels, 1);
-    }
 
-    #[test]
-    fn test_force_close_is_detected() {
-        let node_handle = NodeHandle::new_with_lsp_setup();
-
-        let node = node_handle.start().unwrap();
+        // test multiple txs simultaneously
         let node_id = node.get_node_info().node_pubkey.to_hex();
+        // open 5 channels and force-close 3 of them right away
+        let mut open_channels = open_5_chans_close_2(&node_id);
 
-        let tx_id = nigiri::lnd_node_open_channel(NodeInstance::LspdLnd, &node_id, false).unwrap();
-        try_cmd_repeatedly!(nigiri::mine_blocks, N_RETRIES, HALF_SEC, 50);
-        wait_for_new_channel_to_confirm(NodeInstance::LspdLnd, &node_id);
+        // mine a block and do the same again and remove 1 of the previously opened channels
+        try_cmd_repeatedly!(nigiri::mine_blocks, N_RETRIES, HALF_SEC, 1);
+        nigiri::wait_for_sync(NodeInstance::LspdLnd);
+        let _ = open_5_chans_close_2(&node_id);
+        nigiri::lnd_node_force_close_channel(NodeInstance::LspdLnd, open_channels.pop().unwrap())
+            .unwrap();
 
-        assert_eq!(node.get_node_info().channels_info.num_channels, 1);
-        assert_eq!(node.get_node_info().channels_info.num_usable_channels, 1);
+        try_cmd_repeatedly!(nigiri::mine_blocks, N_RETRIES, HALF_SEC, 10);
+        sleep(Duration::from_secs(10));
 
+        assert_eq!(node.get_node_info().channels_info.num_channels, 4);
+        assert_eq!(node.get_node_info().channels_info.num_usable_channels, 4);
+
+        // test force close is detected
         nigiri::lnd_node_disconnect_peer(NodeInstance::LspdLnd, node_id).unwrap();
         nigiri::lnd_node_force_close_channel(NodeInstance::LspdLnd, tx_id).unwrap();
         nigiri::node_stop(NodeInstance::LspdLnd).unwrap();
 
         sleep(Duration::from_secs(10));
 
-        assert_eq!(node.get_node_info().channels_info.num_channels, 1);
+        assert_eq!(node.get_node_info().channels_info.num_channels, 4);
 
         try_cmd_repeatedly!(nigiri::mine_blocks, N_RETRIES, HALF_SEC, 1);
 
         sleep(Duration::from_secs(10));
 
-        assert_eq!(node.get_node_info().channels_info.num_channels, 0);
+        assert_eq!(node.get_node_info().channels_info.num_channels, 3);
     }
 
     #[test]
-    fn test_channel_remains_usable_after_restart() {
+    fn test_react_to_events_with_offline_node() {
         let node_handle = NodeHandle::new_with_lsp_setup();
 
-        start_node_open_confirm_channel_stop_node(&node_handle);
-
-        let node = node_handle.start().unwrap();
-
-        assert_eq!(node.get_node_info().channels_info.num_channels, 1);
-        assert_eq!(node.get_node_info().channels_info.num_usable_channels, 1);
-    }
-
-    #[test]
-    fn test_channel_is_confirmed_only_after_6_confirmations_offline_node() {
-        let node_handle = NodeHandle::new_with_lsp_setup();
-
-        start_node_open_channel_without_confirm_stop_node(&node_handle);
+        // test channel is confirmed only after 6 confirmations with offline node
+        let tx_id = start_node_open_channel_without_confirm_stop_node(&node_handle);
 
         try_cmd_repeatedly!(nigiri::mine_blocks, N_RETRIES, HALF_SEC, 6);
         // TODO: figure out why the following sleep is needed
         sleep(Duration::from_secs(5));
 
-        let node = node_handle.start().unwrap();
+        {
+            let node = node_handle.start().unwrap();
 
-        assert_eq!(node.get_node_info().channels_info.num_channels, 1);
-        assert_eq!(node.get_node_info().channels_info.num_usable_channels, 1);
-    }
+            assert_eq!(node.get_node_info().channels_info.num_channels, 1);
+            assert_eq!(node.get_node_info().channels_info.num_usable_channels, 1);
+        } // drop node
 
-    #[test]
-    fn test_force_close_is_detected_offline_node() {
-        let node_handle = NodeHandle::new_with_lsp_setup();
+        // test node remains usable after restart
+        {
+            let node = node_handle.start().unwrap();
 
-        let tx_id = start_node_open_confirm_channel_stop_node(&node_handle);
+            assert_eq!(node.get_node_info().channels_info.num_channels, 1);
+            assert_eq!(node.get_node_info().channels_info.num_usable_channels, 1);
+        }
 
+        // test force close is detected offline node
         nigiri::lnd_node_force_close_channel(NodeInstance::LspdLnd, tx_id).unwrap();
         nigiri::node_stop(NodeInstance::LspdLnd).unwrap();
 
@@ -113,9 +110,9 @@ mod chain_sync_test {
 
         let node = node_handle.start().unwrap();
 
+        // Wait for the local node to learn from esplora that the channel has been force closed
         sleep(Duration::from_secs(10));
 
-        // This only passes with the sleep that precedes it. TODO: confirm that's not a problem
         assert_eq!(node.get_node_info().channels_info.num_channels, 0);
     }
 
@@ -136,47 +133,6 @@ mod chain_sync_test {
 
         // This only passes with the sleep that precedes it. TODO: confirm that's not a problem
         assert_eq!(node.get_node_info().channels_info.num_channels, 0);
-    }
-
-    #[test]
-    fn test_multiple_txs_simultaneously() {
-        let node_handle = NodeHandle::new_with_lsp_setup();
-        let node = node_handle.start().unwrap();
-        let node_id = node.get_node_info().node_pubkey.to_hex();
-
-        // open 5 channels and force-close 3 of them right away
-        let mut open_channels = open_5_chans_close_2(&node_id);
-
-        // mine a block and do the same again and remove 1 of the previously opened channels
-        try_cmd_repeatedly!(nigiri::mine_blocks, N_RETRIES, HALF_SEC, 1);
-        nigiri::wait_for_sync(NodeInstance::LspdLnd);
-        let _ = open_5_chans_close_2(&node_id);
-        nigiri::lnd_node_force_close_channel(NodeInstance::LspdLnd, open_channels.pop().unwrap())
-            .unwrap();
-
-        try_cmd_repeatedly!(nigiri::mine_blocks, N_RETRIES, HALF_SEC, 10);
-        sleep(Duration::from_secs(10));
-
-        assert_eq!(node.get_node_info().channels_info.num_channels, 3);
-        assert_eq!(node.get_node_info().channels_info.num_usable_channels, 3);
-    }
-
-    fn start_node_open_confirm_channel_stop_node(node_handle: &NodeHandle) -> String {
-        let node = node_handle.start().unwrap();
-        let node_id = node.get_node_info().node_pubkey.to_hex();
-
-        let tx_id = nigiri::lnd_node_open_channel(NodeInstance::LspdLnd, &node_id, false).unwrap();
-
-        assert_eq!(node.get_node_info().channels_info.num_channels, 1);
-        assert_eq!(node.get_node_info().channels_info.num_usable_channels, 0);
-
-        try_cmd_repeatedly!(nigiri::mine_blocks, N_RETRIES, HALF_SEC, 6);
-        wait_for_new_channel_to_confirm(NodeInstance::LspdLnd, &node_id);
-
-        assert_eq!(node.get_node_info().channels_info.num_channels, 1);
-        assert_eq!(node.get_node_info().channels_info.num_usable_channels, 1);
-
-        tx_id
     }
 
     fn start_node_open_channel_without_confirm_stop_node(node_handle: &NodeHandle) -> String {
