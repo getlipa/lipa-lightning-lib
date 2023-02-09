dabcfeeb59e858f737d81b7baf5b4f8cc4fca332
Andrei <andrei.i@posteo.de>
2023-02-09 00:00:00 +0000

    Set timeouts for gRPC client

--- eel/src/lsp_client/mod.rs
+++ eel/src/lsp_client/mod.rs
@@ -10,11 +10,15 @@ use perro::MapToError;
 use prost::Message;
 use tokio::runtime::{Builder, Runtime};
 use tonic::metadata::{Ascii, MetadataValue};
-use tonic::Request;
+use tonic::service::interceptor::InterceptedService;
+use tonic::service::Interceptor;
+use tonic::transport::channel::Endpoint;
+use tonic::transport::Channel;
+use tonic::{Request, Status};
 
 pub struct LspClient {
-    address: String,
-    bearer: MetadataValue<Ascii>,
+    endpoint: Endpoint,
+    interceptor: AuthInterceptor,
     rt: Runtime,
 }
 
@@ -23,41 +27,46 @@ impl LspClient {
         let bearer = format!("Bearer {auth_token}")
             .parse()
             .map_to_invalid_input("Invalid LSP auth token")?;
+        let interceptor = AuthInterceptor::new(bearer);
+
+        let endpoint = Channel::from_shared(address)
+            .map_to_invalid_input("Invalid gRPC URL")?
+            .connect_timeout(std::time::Duration::from_secs(30))
+            .timeout(std::time::Duration::from_secs(30));
+
         let rt = Builder::new_multi_thread()
             .enable_all()
             .build()
             .map_to_permanent_failure("Failed to build a tokio runtime")?;
+
         Ok(Self {
-            address,
-            bearer,
+            endpoint,
+            interceptor,
             rt,
         })
     }
 
-    fn wrap_request<T>(&self, request: T) -> Request<T> {
-        let mut request = Request::new(request);
-        request
-            .metadata_mut()
-            .insert("authorization", self.bearer.clone());
-        request
+    // The function must be async because `Endpoint::connect_lazy()` access
+    // aync runtime.
+    async fn build_client(
+        &self,
+    ) -> ChannelOpenerClient<InterceptedService<Channel, AuthInterceptor>> {
+        let channel = self.endpoint.connect_lazy();
+        ChannelOpenerClient::with_interceptor(channel, self.interceptor.clone())
     }
 }
 
 impl Lsp for LspClient {
     fn channel_information(&self) -> Result<Vec<u8>> {
-        let request = self.wrap_request(ChannelInformationRequest {
+        let request = Request::new(ChannelInformationRequest {
             pubkey: "".to_string(),
         });
-        let mut client = self
-            .rt
-            .block_on(ChannelOpenerClient::connect(self.address.clone()))
-            .map_to_runtime_error(
-                RuntimeErrorCode::LspServiceUnavailable,
-                "Failed to contact LSP",
-            )?;
         Ok(self
             .rt
-            .block_on(client.channel_information(request))
+            .block_on(async {
+                let mut client = self.build_client().await;
+                client.channel_information(request).await
+            })
             .map_to_runtime_error(
                 RuntimeErrorCode::LspServiceUnavailable,
                 "LSP channel information request failed",
@@ -67,20 +76,36 @@ impl Lsp for LspClient {
     }
 
     fn register_payment(&self, blob: Vec<u8>) -> Result<()> {
-        let request = self.wrap_request(RegisterPaymentRequest { blob });
-        let mut client = self
-            .rt
-            .block_on(ChannelOpenerClient::connect(self.address.clone()))
-            .map_to_runtime_error(
-                RuntimeErrorCode::LspServiceUnavailable,
-                "Failed to contact LSP",
-            )?;
+        let request = Request::new(RegisterPaymentRequest { blob });
         self.rt
-            .block_on(client.register_payment(request))
+            .block_on(async {
+                let mut client = self.build_client().await;
+                client.register_payment(request).await
+            })
             .map_to_runtime_error(
                 RuntimeErrorCode::LspServiceUnavailable,
                 "LSP register payment request failed",
             )?;
         Ok(())
+    }
+}
+
+#[derive(Clone)]
+struct AuthInterceptor {
+    bearer: MetadataValue<Ascii>,
+}
+
+impl AuthInterceptor {
+    fn new(bearer: MetadataValue<Ascii>) -> Self {
+        Self { bearer }
+    }
+}
+
+impl Interceptor for AuthInterceptor {
+    fn call(&mut self, mut request: Request<()>) -> std::result::Result<Request<()>, Status> {
+        request
+            .metadata_mut()
+            .insert("authorization", self.bearer.clone());
+        Ok(request)
     }
 }
