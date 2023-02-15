2ce4abaf94fb1fced8bb75491c37a1465bf83113
Daniel Granhão <daniel.granhao@gmail.com>
2023-02-15 15:00:04 +0000

    Fix get payment queries

--- eel/src/payment_store.rs
+++ eel/src/payment_store.rs
@@ -211,18 +211,8 @@ impl PaymentStore {
             .prepare("\
             SELECT payments.payment_id, payments.type, hash, preimage, amount_msat, network_fees_msat, lsp_fees_msat, invoice, metadata, recent_events.type as state, recent_events.inserted_at, description, creation_events.inserted_at \
             FROM payments \
-            JOIN ( \
-                SELECT * \
-                FROM events \
-                GROUP BY payment_id \
-                HAVING MAX(event_id) \
-            ) AS recent_events ON payments.payment_id=recent_events.payment_id \
-            JOIN ( \
-                SELECT * \
-                FROM events \
-                GROUP BY payment_id \
-                HAVING MIN(event_id) \
-            ) AS creation_events ON payments.payment_id=creation_events.payment_id \
+            JOIN recent_events ON payments.payment_id=recent_events.payment_id \
+            JOIN creation_events ON payments.payment_id=creation_events.payment_id \
             ORDER BY payments.payment_id DESC \
             LIMIT ? \
             ")
@@ -245,18 +235,8 @@ impl PaymentStore {
             .prepare("\
             SELECT payments.payment_id, payments.type, hash, preimage, amount_msat, network_fees_msat, lsp_fees_msat, invoice, metadata, recent_events.type as state, recent_events.inserted_at, description, creation_events.inserted_at \
             FROM payments \
-            JOIN ( \
-                SELECT * \
-                FROM events \
-                GROUP BY payment_id \
-                HAVING MAX(event_id) \
-            ) AS recent_events ON payments.payment_id=recent_events.payment_id \
-            JOIN ( \
-                SELECT * \
-                FROM events \
-                GROUP BY payment_id \
-                HAVING MIN(event_id) \
-            ) AS creation_events ON payments.payment_id=creation_events.payment_id \
+            JOIN recent_events ON payments.payment_id=recent_events.payment_id \
+            JOIN creation_events ON payments.payment_id=creation_events.payment_id \
             WHERE payments.hash=? \
             ")
             .map_to_permanent_failure("Failed to prepare SQL query")?;
@@ -334,6 +314,24 @@ fn apply_migrations(db_conn: &Connection) -> Result<()> {
               inserted_at INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
               FOREIGN KEY (payment_id) REFERENCES payments(payment_id)
             );
+            CREATE VIEW IF NOT EXISTS creation_events
+            AS
+            SELECT *
+            FROM events
+            JOIN (
+                SELECT payment_id, MIN(event_id) AS min_event_id
+                FROM events
+                GROUP BY payment_id
+            ) AS min_ids ON min_event_id=events.event_id;
+            CREATE VIEW IF NOT EXISTS recent_events
+            AS
+            SELECT *
+            FROM events
+            JOIN (
+                SELECT payment_id, MAX(event_id) AS max_event_id
+                FROM events
+                GROUP BY payment_id
+            ) AS max_ids ON max_event_id=events.event_id;
         ",
         )
         .map_to_permanent_failure("Failed to set up local payment database")
