8503604812e747d2ad4896ddbcae461aa4cbfd12
Andrei <92177534+andrei-21@users.noreply.github.com>
2023-03-16 15:46:01 +0000

    User event callback does not return errors (#269)

--- src/callbacks.rs
+++ src/callbacks.rs
@@ -1,40 +1,9 @@
-use std::fmt::Debug;
-use uniffi::UnexpectedUniFFICallbackError;
-
-#[derive(Debug, thiserror::Error)]
-pub enum CallbackError {
-    #[error("InvalidInput")]
-    InvalidInput,
-
-    #[error("RuntimeError")]
-    RuntimeError,
-
-    #[error("PermanentFailure")]
-    PermanentFailure,
-
-    #[error("UnexpectedUniFFICallbackError")]
-    UnexpectedUniFFI,
-}
-
-impl From<UnexpectedUniFFICallbackError> for CallbackError {
-    fn from(_error: UnexpectedUniFFICallbackError) -> Self {
-        CallbackError::UnexpectedUniFFI
-    }
-}
-
-pub type CallbackResult<T> = Result<T, CallbackError>;
-
 pub trait EventsCallback: Send + Sync {
-    fn payment_received(&self, payment_hash: String, amount_msat: u64) -> CallbackResult<()>;
+    fn payment_received(&self, payment_hash: String, amount_msat: u64);
 
-    fn channel_closed(&self, channel_id: String, reason: String) -> CallbackResult<()>;
+    fn channel_closed(&self, channel_id: String, reason: String);
 
-    fn payment_sent(
-        &self,
-        payment_hash: String,
-        payment_preimage: String,
-        fee_paid_msat: u64,
-    ) -> CallbackResult<()>;
+    fn payment_sent(&self, payment_hash: String, payment_preimage: String, fee_paid_msat: u64);
 
-    fn payment_failed(&self, payment_hash: String) -> CallbackResult<()>;
+    fn payment_failed(&self, payment_hash: String);
 }
