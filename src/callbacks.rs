cbe9e5b3022f81e746deac767d8443d4c7dfcd0e
Andrei <92177534+andrei-21@users.noreply.github.com>
2023-05-30 10:40:42 +0100

    Remove amounts from events callback (#400)

--- src/callbacks.rs
+++ src/callbacks.rs
@@ -1,9 +1,9 @@
 pub trait EventsCallback: Send + Sync {
-    fn payment_received(&self, payment_hash: String, amount_msat: u64);
+    fn payment_received(&self, payment_hash: String);
 
     fn channel_closed(&self, channel_id: String, reason: String);
 
-    fn payment_sent(&self, payment_hash: String, payment_preimage: String, fee_paid_msat: u64);
+    fn payment_sent(&self, payment_hash: String, payment_preimage: String);
 
     fn payment_failed(&self, payment_hash: String);
 }
--- tests/print_events_handler/mod.rs
+++ tests/print_events_handler/mod.rs
@@ -4,30 +4,19 @@ use uniffi_lipalightninglib::EventsCallback;
 pub struct PrintEventsHandler {}
 
 impl EventsCallback for PrintEventsHandler {
-    fn payment_received(&self, payment_hash: String, amount_msat: u64) {
-        info!(
-            "Received a payment! Value of {} milli satoshis and payment hash is {}",
-            amount_msat, payment_hash
-        );
+    fn payment_received(&self, payment_hash: String) {
+        info!("Received a payment with payment hash {payment_hash}");
     }
 
     fn channel_closed(&self, channel_id: String, reason: String) {
-        info!(
-            "A channel was closed! Channel ID {} was closed due to {}",
-            channel_id, reason
-        );
+        info!("A channel was closed! Channel ID {channel_id} was closed due to {reason}");
     }
 
-    fn payment_sent(&self, payment_hash: String, payment_preimage: String, fee_paid_msat: u64) {
-        info!(
-            "A payment has been successfully sent! Its preimage is {}, the hash is {}, and a total of {} msats were paid in lightning fees",
-            payment_preimage,
-            payment_hash,
-            fee_paid_msat
-        );
+    fn payment_sent(&self, payment_hash: String, payment_preimage: String) {
+        info!("A payment has been successfully sent! Its preimage is {payment_preimage}, the hash is {payment_hash}");
     }
 
     fn payment_failed(&self, payment_hash: String) {
-        info!("A payment has failed! Its hash is {}", payment_hash);
+        info!("A payment has failed! Its hash is {payment_hash}");
     }
 }
