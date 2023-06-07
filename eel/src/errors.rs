f0ed2c885eef48a1d7a9b1fc98082ae1753fbae5
Gabriel Comte <gabriel.comte@gmail.com>
2023-06-07 15:32:18 +0200

    Don't expose ErrorTypes in UDL that cannot occur

--- eel/src/errors.rs
+++ eel/src/errors.rs
@@ -7,8 +7,6 @@ pub enum RuntimeErrorCode {
     ExchangeRateProviderUnavailable,
     LspServiceUnavailable,
     RemoteStorageError,
-    RgsServiceUnavailable,
-    RgsUpdateError,
 
     NonExistingWallet,
 
@@ -22,12 +20,26 @@ pub enum RuntimeErrorCode {
     ObjectNotFound,
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub enum InternalErrorCode {
+    RgsServiceUnavailable, // The rapid gossip sync service is unavailable. Could there be a loss of internet connection?
+    RgsUpdateError,        // Failed to apply update. Maybe retry?
+}
+
 impl Display for RuntimeErrorCode {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{self:?}")
     }
 }
 
+impl Display for InternalErrorCode {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
 pub type Error = perro::Error<RuntimeErrorCode>;
+pub type InternalError = perro::Error<InternalErrorCode>;
 
 pub type Result<T> = std::result::Result<T, perro::Error<RuntimeErrorCode>>;
+pub type InternalResult<T> = std::result::Result<T, perro::Error<InternalErrorCode>>;
