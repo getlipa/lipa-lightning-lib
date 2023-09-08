5064bc8e3aadd43d2c004ab10bd14c330e553ed4
Gabriel Comte <gabriel.comte@gmail.com>
2023-09-08 16:20:32 +0200

    Revert "Make Breez SDK invite code configurable"
    This reverts commit a555e3dea53056ed9193de5d2758c5e866e530dc.

--- examples/3l-node/main.rs
+++ examples/3l-node/main.rs
@@ -30,7 +30,6 @@ fn main() {
     let config = Config {
         environment,
         seed,
-        invite_code: None,
         fiat_currency: "EUR".to_string(),
         local_persistence_path: base_dir.clone(),
         timezone_config: TzConfig {
