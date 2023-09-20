ec47f8dd3274c1bbb7fb8f03d16b39bbe6ca849c
Andrei <92177534+andrei-21@users.noreply.github.com>
2023-09-20 10:41:43 +0100

    Clean integration tests (#654)

--- tests/node_info_test.rs
+++ tests/node_info_test.rs
@@ -1,32 +1,28 @@
-#[path = "print_events_handler/mod.rs"]
 mod print_events_handler;
-mod setup_3l;
+mod setup;
 
-#[cfg(feature = "nigiri")]
-mod node_info_test {
-    use crate::setup_3l::NodeHandle;
-    use std::str::FromStr;
+use crate::setup::NodeHandle;
 
-    use bitcoin::secp256k1::PublicKey;
-    use serial_test::file_serial;
+use bitcoin::secp256k1::PublicKey;
+use serial_test::file_serial;
+use std::str::FromStr;
 
-    #[test]
-    #[file_serial(key, "/tmp/3l-int-tests-lock")]
-    fn test_get_node_info() {
-        let node = NodeHandle::new().start().unwrap();
-        let node_info = node.get_node_info().unwrap();
+#[test]
+#[file_serial(key, "/tmp/3l-int-tests-lock")]
+fn test_get_node_info() {
+    let node = NodeHandle::new().start().unwrap();
+    let node_info = node.get_node_info().unwrap();
 
-        assert!(
-            PublicKey::from_str(&*node_info.node_pubkey).is_ok(),
-            "Node public key is not valid"
-        );
-        assert!(
-            node_info.channels_info.num_channels >= node_info.channels_info.num_usable_channels,
-            "Number of channels must be greater or equal to number of usable channels"
-        );
-        assert!(
-            node_info.channels_info.local_balance.sats < 21_000_000 * 100_000_000,
-            "Node must not hold more than 21 million BTC"
-        );
-    }
+    assert!(
+        PublicKey::from_str(&*node_info.node_pubkey).is_ok(),
+        "Node public key is not valid"
+    );
+    assert!(
+        node_info.channels_info.num_channels >= node_info.channels_info.num_usable_channels,
+        "Number of channels must be greater or equal to number of usable channels"
+    );
+    assert!(
+        node_info.channels_info.local_balance.sats < 21_000_000 * 100_000_000,
+        "Node must not hold more than 21 million BTC"
+    );
 }
