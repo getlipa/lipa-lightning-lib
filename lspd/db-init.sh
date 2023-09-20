a78295c79c99ec730746c4dbf919ecc6e841d206
Andrei <andrei.i@posteo.de>
2023-09-20 00:00:00 +0000

    Remove lspd and rgs

--- lspd/db-init.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-for sql_file in /migrations/*.up.sql; do
-    echo "Applying $sql_file"
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < $sql_file
-done
