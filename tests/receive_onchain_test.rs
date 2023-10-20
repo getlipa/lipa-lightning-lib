0a6554b4ee2c3b3f8d425cb9c248d92201d27f45
Daniel Granhão <daniel.granhao@gmail.com>
2023-10-20 19:08:48 +0100

    Implement `generate_swap_address()`

---
+++ tests/receive_onchain_test.rs
@@ -0,0 +1,25 @@
+mod print_events_handler;
+mod setup;
+
+use crate::setup::start_alice;
+
+use serial_test::file_serial;
+
+#[test]
+#[file_serial(key, "/tmp/3l-int-tests-lock")]
+fn test_receive_onchain() {
+    let node = start_alice().unwrap();
+
+    let swap_info = node.generate_swap_address(None).unwrap();
+    assert!(swap_info.address.starts_with("bc1"));
+    assert!(swap_info.min_deposit.sats < swap_info.max_deposit.sats);
+
+    // Calling a second time isn't an issue because no swap has been started
+    let swap_info = node.generate_swap_address(None).unwrap();
+    assert!(swap_info.address.starts_with("bc1"));
+    assert!(swap_info.min_deposit.sats < swap_info.max_deposit.sats);
+
+    let lsp_fee_params = node.calculate_lsp_fee(100000).unwrap().lsp_fee_params;
+    let swap_info = node.generate_swap_address(lsp_fee_params).unwrap();
+    assert!(swap_info.address.starts_with("bc1"));
+}
