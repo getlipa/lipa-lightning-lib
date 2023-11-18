0490248a1dcdb53b1c51ee63dffaef53c1c8b811
Andrei <92177534+andrei-21@users.noreply.github.com>
2023-11-18 09:28:30 +0000

    Promote `LogIgnoreError` (#773)

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,19 @@
+use log::{log, Level};
 use std::time::{Duration, SystemTime};
 
 pub(crate) fn unix_timestamp_to_system_time(timestamp: u64) -> SystemTime {
     let duration = Duration::from_secs(timestamp);
     SystemTime::UNIX_EPOCH + duration
+}
+
+pub(crate) trait LogIgnoreError {
+    fn log_ignore_error(self, level: Level, message: &str);
+}
+
+impl<T, E: std::fmt::Display> LogIgnoreError for Result<T, E> {
+    fn log_ignore_error(self, level: Level, message: &str) {
+        if let Err(e) = self {
+            log!(level, "{message}: {e}")
+        }
+    }
 }
