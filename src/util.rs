44b7d31dbc7e8de7cd6d8d75a54d6f6697c499cc
Gabriel Comte <gabriel.comte@gmail.com>
2023-12-20 15:38:26 +0100

    Log hashes in channels list as hex strings

--- src/util.rs
+++ src/util.rs
@@ -1,9 +1,28 @@
+use hex::encode;
 use log::{log, Level};
+use regex::Regex;
 use std::time::{Duration, SystemTime};
 
 pub(crate) fn unix_timestamp_to_system_time(timestamp: u64) -> SystemTime {
     let duration = Duration::from_secs(timestamp);
     SystemTime::UNIX_EPOCH + duration
+}
+
+// Replaces all occurrences of byte arrays with their hex representation:
+// 'Hello [15, 16, 255] world' -> 'Hello "0f10ff" world'
+pub(crate) fn replace_byte_arrays_by_hex_string(original: &str) -> String {
+    let byte_array_pattern = Regex::new(r"\[([\d\s,]+)\]").unwrap();
+
+    byte_array_pattern
+        .replace_all(original, |caps: &regex::Captures| {
+            let byte_array = caps.get(1).unwrap().as_str();
+            let hex_data = byte_array
+                .split(',')
+                .map(|byte| u8::from_str_radix(byte.trim(), 10).unwrap())
+                .collect::<Vec<u8>>();
+            format!("\"{}\"", encode(&hex_data))
+        })
+        .to_string()
 }
 
 pub(crate) trait LogIgnoreError {
