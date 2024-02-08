02dd5f157e8f12379fb5ee40821e63204c67df43
Andrei <andrei.i@posteo.de>
2024-02-08 00:00:00 +0000

    Handle capital letters

--- parser/examples/demo/main.rs
+++ parser/examples/demo/main.rs
@@ -1,27 +1,22 @@
 use crossterm::cursor::MoveToColumn;
 use crossterm::event::{read, Event, KeyCode, KeyModifiers};
 use crossterm::execute;
-use crossterm::style::{Print, Stylize};
+use crossterm::style::Stylize;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 use parser::{parse_lightning_address, ParseError};
 use std::io;
 use std::io::Write;
 
-fn parse_and_print(line: &str) -> io::Result<()> {
-    let mut stdout = io::stdout();
+fn parse_and_print(line: &str) {
     match parse_lightning_address(&line) {
-        Ok(()) => execute!(stdout, Print(line.green())),
-        Err(ParseError::Incomplete) => execute!(stdout, Print(line.white())),
-        Err(ParseError::ExcessSuffix(at)) => execute!(
-            stdout,
-            Print(line[..at].green()),
-            Print(line[at..].black().on_magenta())
-        ),
-        Err(ParseError::UnexpectedCharacter(at)) => execute!(
-            stdout,
-            Print(line[..at].white()),
-            Print(line[at..].black().on_red())
-        ),
+        Ok(()) => print!("{}", line.green()),
+        Err(ParseError::Incomplete) => print!("{}", line.white()),
+        Err(ParseError::ExcessSuffix(at)) => {
+            print!("{}{}", line[..at].green(), line[at..].black().on_magenta())
+        }
+        Err(ParseError::UnexpectedCharacter(at)) => {
+            print!("{}{}", line[..at].white(), line[at..].black().on_red())
+        }
     }
 }
 
@@ -32,18 +27,22 @@ fn print_events() -> io::Result<()> {
 
     let mut line = String::new();
     loop {
-        let event = read()?;
-        if let Event::Key(key_event) = event {
+        if let Event::Key(key_event) = read()? {
             match key_event.code {
                 KeyCode::Char(c) if key_event.modifiers == KeyModifiers::NONE => {
                     execute!(stdout, Clear(ClearType::CurrentLine), MoveToColumn(0))?;
                     line.push(c);
-                    parse_and_print(&line)?;
+                    parse_and_print(&line);
+                }
+                KeyCode::Char(c) if key_event.modifiers == KeyModifiers::SHIFT => {
+                    execute!(stdout, Clear(ClearType::CurrentLine), MoveToColumn(0))?;
+                    line.push(c.to_ascii_uppercase());
+                    parse_and_print(&line);
                 }
                 KeyCode::Backspace => {
                     execute!(stdout, Clear(ClearType::CurrentLine), MoveToColumn(0))?;
                     line.pop();
-                    parse_and_print(&line)?;
+                    parse_and_print(&line);
                 }
                 KeyCode::Enter => {
                     line.clear();
@@ -64,13 +63,13 @@ fn print_events() -> io::Result<()> {
 fn main() -> io::Result<()> {
     println!("{}", "Press ESC to exit".bold());
     println!();
-    println!("       Valid complete is {}", "green".green());
-    println!("         Valid prefix is {}", "white".white());
+    println!("        Valid complete is {}", "green".green());
+    println!("          Valid prefix is {}", "white".white());
     println!(
-        "     Excess suffix is on {}",
-        "magneta".black().on_magenta()
+        "      Excess suffix is on {}",
+        "magenta".black().on_magenta()
     );
-    println!("Invalid charactes are on {}", "red".black().on_red());
+    println!("Invalid characters are on {}", "red".black().on_red());
     println!();
 
     enable_raw_mode()?;
