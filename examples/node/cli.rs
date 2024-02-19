546b766e51e3cd589745443a00f3a2353813ccc7
Gabriel Comte <gabriel.comte@gmail.com>
2024-02-19 17:12:01 +0100

    Implement satsymbol.org for CLI wallet

--- examples/node/cli.rs
+++ examples/node/cli.rs
@@ -539,7 +539,7 @@ fn get_exchange_rate(node: &LightningNode) {
         Some(r) => {
             let dt: DateTime<Utc> = r.updated_at.into();
             println!(
-                "{}: {} SAT - updated at {} UTC",
+                "{}: ₿ {} - updated at {} UTC",
                 r.currency_code,
                 r.rate,
                 dt.format("%d/%m/%Y %T")
@@ -1266,7 +1266,7 @@ fn amount_to_string(amount: &Amount) -> String {
         Some(fiat) => fiat_value_to_string(fiat),
         None => "exchange rate unknown".to_string(),
     };
-    format!("{} SAT ({fiat})", amount.sats)
+    format!("₿ {} ({fiat})", amount.sats)
 }
 
 fn get_registered_topup(node: &LightningNode) -> Result<()> {
