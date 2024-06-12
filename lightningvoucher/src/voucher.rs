09b9e7335ebe5ccfa1711b2059e4c48186927fb5
Andrei <andrei.i@posteo.de>
2024-06-12 00:00:00 +0000

    Verify signature

--- lightningvoucher/src/voucher.rs
+++ lightningvoucher/src/voucher.rs
@@ -142,24 +142,27 @@ impl Voucher {
 
     pub fn decrypt(redeemer_key: PublicKey, data: &[u8]) -> Self {
         const NONCE_SIZE: usize = <Aes256Gcm as AeadCore>::NonceSize::USIZE;
-        if data.len() < NONCE_SIZE {
+        const SIGNATURE_SIZE: usize = 64;
+        if data.len() < NONCE_SIZE + SIGNATURE_SIZE {
             panic!("data is too short");
         }
 
-        let signature_start = data.len() - 64;
+        let signature_start = data.len() - SIGNATURE_SIZE;
         let (data, signature) = data.split_at(signature_start);
+        let signature = Signature::from_compact(signature).unwrap();
 
         let nonce_start = data.len() - NONCE_SIZE;
         let (metadata, nonce) = data.split_at(nonce_start);
 
         let nonce = Nonce::from_slice(nonce);
         let cipher = Aes256Gcm::new(&symmetric_key(&redeemer_key));
-        let metadata = cipher.decrypt(nonce, metadata).unwrap();
-        let metadata = String::from_utf8(metadata).unwrap();
-        let metadata: VoucherMetadata = serde_json::from_str(&metadata).unwrap();
+        let metadata_json = cipher.decrypt(nonce, metadata).unwrap();
+        let message = Message::from_hashed_data::<secp256k1::hashes::sha256::Hash>(&metadata_json);
+        SECP256K1
+            .verify_ecdsa(&message, &signature, &redeemer_key)
+            .unwrap();
 
-        let signature = Signature::from_compact(signature).unwrap();
-        // TODO: Verify signature.
+        let metadata: VoucherMetadata = serde_json::from_slice(&metadata_json).unwrap();
 
         Self {
             redeemer_key,
@@ -186,24 +189,24 @@ mod tests {
         // Client.
         let (voucher, _issuer_key) =
             Voucher::generate((10, 12), "Descr".to_string(), Duration::from_secs(60));
-        println!("{voucher:?}");
-        // Store voucher to the local db.
+        println!("Voucher: {voucher:?}");
+        // Store voucher and issuer_key to the local db.
         let encrypted = voucher.encrypt();
-        println!("{}", encrypted.hash);
+        println!("Hash: {}", encrypted.hash);
         // Register encrypted voucher on the server.
         let lnurl_prefix = "https://zzd.es/lnurl/";
         let lnurl = encode(&voucher, lnurl_prefix);
-        println!("{lnurl}");
+        println!("LNURL: {lnurl}");
         // Send lnurl to the recipient.
 
         // Server.
         let redeemer_key = decode(&lnurl);
         let hash = key_to_hash(&redeemer_key);
-        println!("{hash}");
-        // Look up encrypted metadata by hash.
-        let v = Voucher::decrypt(redeemer_key, &encrypted.data);
-        println!("{v:?}");
-        let r = to_lnurl_response(&v, lnurl_prefix.to_string());
-        println!("{r}");
+        println!("Hash: {hash}");
+        // Look up encrypted data by hash.
+        let voucher = Voucher::decrypt(redeemer_key, &encrypted.data);
+        println!("Voucher: {voucher:?}");
+        let response = to_lnurl_response(&voucher, lnurl_prefix.to_string());
+        println!("Response: {response}");
     }
 }
